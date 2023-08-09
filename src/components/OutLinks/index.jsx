8c9c4aec1075b193b5d4fb1098ad368b3aead09f
jiaolong <jiaolong112@234.com>
2023-08-09 10:50:45 +0800

    feat:add github icon

--- src/components/OutLinks/index.jsx
+++ src/components/OutLinks/index.jsx
@@ -2,18 +2,22 @@ import "./index.scss";
 import { ReactComponent as Twitter } from "fonts/svg/twitter.svg";
 import { ReactComponent as Telegram } from "fonts/svg/telegram.svg";
 import { ReactComponent as Gitbook } from "fonts/svg/gitbook.svg";
+import { ReactComponent as Git } from "fonts/svg/github.svg";
 export default function OutLinks() {
   return (
     <>
       <div className="outLinks">
-        {/* <a href="https://t.me/nostrswap" target="_blank">
+        <a href="https://t.me/nostrassets" target="_blank">
           <Telegram className="svg-icon" width={30} height={30}></Telegram>
         </a>
-        <a href="https://twitter.com/nostrswap" target="_blank">
+        <a href="https://twitter.com/nostrassets" target="_blank">
           <Twitter className="svg-icon" width={30} height={30}></Twitter>
-        </a> */}
+        </a>
         <a href="https://doc.nostrassets.com/" target="_blank">
-          <Gitbook className="svg-icon" width={26} height={26}></Gitbook>
+          <Gitbook className="svg-icon" style={{ width: "30px", height: "30px" }}></Gitbook>
+        </a>
+        <a href="https://github.com/nostrassets/interface/#readme" target="_blank">
+          <Git className="svg-icon" width={30} height={30} />
         </a>
       </div>
     </>
