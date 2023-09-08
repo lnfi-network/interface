a188842c57ccab6dc5ed32a5c4189b0dfcd3bd3e
jiaolong <jiaolong112@234.com>
2023-09-08 14:43:25 +0800

    fix:switch bug

--- src/hooks/useListenerAlbyAccountChange.js
+++ src/hooks/useListenerAlbyAccountChange.js
@@ -1,18 +1,25 @@
 import { useSelector, useDispatch } from "react-redux"
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { initNostrAccount } from "store/reducer/userReducer";
 const useListenerAlbyAccountChange = () => {
-  const { nostrAccount } = useSelector(({ user }) => user);
   const dispatch = useDispatch()
+
   const albyAccountChange = useCallback(async () => {
     dispatch(initNostrAccount(''));
     const albyNostrAccount = await window.nostr.getPublicKey();
     dispatch(initNostrAccount(albyNostrAccount));
   }, [dispatch])
+
+
   useEffect(() => {
-    if (window.nostr) {
-      window.nostr.on('accountChanged', albyAccountChange)
-      return () => {
+    setTimeout(() => {
+      if (window.nostr) {
+        window.nostr.on('accountChanged', albyAccountChange)
+      }
+    }, 1000)
+
+    return () => {
+      if (window.nostr) {
         window.nostr.off('accountChanged', albyAccountChange)
       }
     }
