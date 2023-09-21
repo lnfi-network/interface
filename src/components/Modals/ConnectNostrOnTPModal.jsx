edd25028a05408102b7f2be442e21bcedb6c2042
jiaolong <jiaolong112@234.com>
2023-09-21 15:55:13 +0800

    fix:bug

--- src/components/Modals/ConnectNostrOnTPModal.jsx
+++ src/components/Modals/ConnectNostrOnTPModal.jsx
@@ -38,21 +38,17 @@ export default function ConnectNostrOnTPModal() {
             Similar to other Web3 websites, using NostrAssets requires connecting your Nostr account to approve each
             account operation.
           </p>
-          <p className="connect-nostr-modal-description mt10 f12">There are currently two ways to connect Nostr:</p>
-          <p className="connect-nostr-modal-description b mt10 f12">
-            1. Visit the URL on Web and connect Nostr with website extensions that supports Nostr and manage your Nostr
-            private key. eg. Alby extension, which is referred to as the Metamask for Nostr.
-          </p>
-          {/* <p className="connect-nostr-modal-description f10">
+          <p className="connect-nostr-modal-description mt10 f12">2 ways to connect NostrAssets to Nostr:</p>
+          <p className="connect-nostr-modal-description b mt10 f12">1. Connect via Nostr Web Browser Extensions</p>
+          <p className="connect-nostr-modal-description f10">
             Eg. Alby Browser Extension which is referred to as the Metamask for Nostr
-          </p> */}
-          <p className="connect-nostr-modal-description b mt10 f12">
-            2. Use a wallet that supports Nostr to connect on the mobile phone and manage your Nostr private key.
-            Currently only TP Wallet is supported.
           </p>
-          {/* <p className="connect-nostr-modal-description f10">
+          <p className="connect-nostr-modal-description b mt10 f12">
+            2. Use a Nostr-compatible wallet on your mobile device
+          </p>
+          <p className="connect-nostr-modal-description f10">
             Currently only TP Wallet is the sole supported option for this method.
-          </p> */}
+          </p>
           <div className="connect-nostr-modal-btn">
             {/* <ConnectWalletButton imgSrc={IconTPWallet} href={`tpdapp://open?params=${encodeTPParams}`}>
               Open in TP Wallet
