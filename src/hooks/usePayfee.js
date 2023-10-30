26bddaab7e02cb570bf9a2fee907e6c74a504267
jiaolong <jiaolong112@234.com>
2023-10-30 14:13:08 +0800

    add usePayfee

---
+++ src/hooks/usePayfee.js
@@ -0,0 +1,81 @@
+import { useSelector } from "react-redux";
+import { buildPSBT, sha256 } from "lib/buildPsbt/buildPsbt";
+import { validate, getAddressInfo } from 'bitcoin-address-validation';
+import { useCallback } from 'react'
+const getBuildPSBTResult = async (eventId, fee, account) => {
+  const networkstr = await window.unisat.getNetwork();
+  const publicKey = await window.unisat.getPublicKey();
+  const memeList = [eventId];
+
+  return await buildPSBT(
+    networkstr,
+    publicKey,
+    memeList,
+    [
+      { value: 1000, address: process.env.REACT_APP_GAS_ADDR },
+      { value: 200, address: process.env.REACT_APP_TREASURY_ADDR }
+    ],
+    account,
+    fee,
+  );
+};
+export const useUnisatPayfee = () => {
+  const { account, chainId } = useSelector(({ user }) => user);
+  const handleUnisatPay = useCallback(async (buildParam, needEncode = false) => {
+    // todo show feeRateModal
+    let sendTx = "";
+    if (!window.unisat) {
+      throw new Error("No unisat provider.");
+    }
+    if (!validate(account)) {
+      throw new Error("Invalid account");
+    }
+    const addressInfo = getAddressInfo(account)
+    if (addressInfo.type !== 'p2tr') {
+      throw new Error("Invalid account,Please switch your address type to P2RT.")
+    }
+    if (process.env.REACT_APP_CURRENT_ENV === 'prod') {
+      if (chainId === 'testnet') {
+        throw new Error("Please switch the network to mainnet.")
+      }
+    } else {
+      if (chainId === 'mainnet') {
+        throw new Error("Please switch the network to testnet.")
+      }
+    }
+    // check balance
+    const balance = await window.unisat.getBalance().catch(e => {
+      return {
+        confirmed: 0,
+        unconfirmed: 0
+      }
+    });
+
+    if (balance?.confirmed === 0) {
+      throw new Error("Insufficient Balance1.")
+    }
+    const willBuildParam = needEncode ? sha256(buildParam) : buildParam
+
+    let feeRate = 5;
+    let dummy = await getBuildPSBTResult(willBuildParam, 5000, account);
+    let estimateFee = dummy.bytesize * feeRate;
+
+    const constructPsbtRet = await getBuildPSBTResult(willBuildParam, estimateFee, account);
+    if (!constructPsbtRet) {
+      throw new Error("Create Psbt failed.");
+    }
+    const { signList, unsignedHex } = constructPsbtRet;
+    const signedPsbt = await window.unisat.signPsbt(unsignedHex, {
+      autoFinalized: true,
+      toSignInputs: [...signList]
+    });
+
+    if (signedPsbt) {
+      sendTx = await window.unisat.pushPsbt(signedPsbt);
+    }
+    return sendTx;
+  }, [account, chainId]);
+  return {
+    handleUnisatPay
+  }
+}
